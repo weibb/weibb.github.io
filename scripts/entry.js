9d2e80776c693df7c8934e2617d1545eb71d6108
weixh <quiteripping@gmail.com>
2017-09-05 15:37:14 -0400

    Add site

---
+++ scripts/entry.js
@@ -0,0 +1,80 @@
+const pubs = [
+    {
+        id: 1,
+        title: 'Development of A Custom Imaging System for Simultaneous Monitoring of Retinal and Cortical Physiology',
+        publication: 'Association for Research in Vision and Ophthalmology, Annual Meeting',
+        authors: [
+            {
+                name: 'Gehlbach GL',
+            },
+            {
+                name: 'Taylor R',
+            },
+            {
+                name: 'Mori K',
+            },
+        ],
+        category: ['basic'],
+        type: ['caseReport', 'poster'],
+        link: 'http://google.com',
+        date: '2009',
+        image: 'https://upload.wikimedia.org/wikipedia/en/thumb/0/09/Circle_Logo.svg/1024px-Circle_Logo.svg.png',
+    },
+    {
+        id: 2,
+        title: 'Non-Diabetic Ocular Abnormalities Detected by Telemedicine Diabetic Retinopathy Assessment Technology in Safety-Net Medical Clinics',
+        publication: '48th Annual Retina Society Meeting',
+        authors: [
+            {
+                name: 'Zimmer-Geller I',
+            },
+            {
+                name: 'Gehlbach PL',
+            },
+        ],
+        category: ['clinical'],
+        type: ['collaborativeResearch', 'abstract'],
+        link: 'http://google.com',
+        date: '2012',
+        image: 'https://upload.wikimedia.org/wikipedia/en/thumb/0/09/Circle_Logo.svg/1024px-Circle_Logo.svg.png',
+    },
+    {
+        id: 3,
+        title: 'Noninvasive imaging of retinal blood vessels and blood flow using a custom, non-mydriatic, handheld optical imager designed for use in primary care and telehealth settings',
+        publication: 'ARVO',
+        authors: [
+            {
+                name: 'Rege A',
+            },
+            {
+                name: 'Raje K',
+            },
+            {
+                name: 'Kheng M',
+            },
+        ],
+        category: ['engineering'],
+        type: ['collaborativeResearch', 'proceedingReport'],
+        link: 'http://google.com',
+        date: '2015',
+        image: 'https://upload.wikimedia.org/wikipedia/en/thumb/0/09/Circle_Logo.svg/1024px-Circle_Logo.svg.png',
+    },
+    {
+        id: 4,
+        title: 'Evaluation of Motion Artifact Associated with Use of a Custom, Handheld Imaging Device for Non-Invasive Retinal Blood Flow (RBF) Measurements in Telehealth Settings',
+        publication: 'ARVO',
+        authors: [
+            {
+                name: 'Brooke M',
+            },
+            {
+                name: 'Cheng T',
+            },
+        ],
+        category: ['clinical'],
+        type: ['collaborativeResearch', 'abstract'],
+        link: 'http://google.com',
+        date: '2015',
+        image: 'https://upload.wikimedia.org/wikipedia/en/thumb/0/09/Circle_Logo.svg/1024px-Circle_Logo.svg.png',
+    },
+];
